667e5c3b53538af29dfbd275bd90bb97cfc95dd4
ClarenceKiva <Clarenceaureliopribadi@gmail.com>
2023-03-22 19:39:09 +0700

    added new codei into app.js and added a .env file

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require('express');
 const mysql = require('mysql');
 const dotenv = require("dotenv").config();
 const app = express();
-const PORT = 3000; 
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("GET Request Called")
-})
- 
-app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
+app.use(express.json());
+const port = process.env.PORT || 8080;
+app.listen(port, ()=> {
+    console.log(`API listening on port ${port}`);
 });
+
+app.get("/", async (req, res) =>{
+    res.json({status: "API is Running and functioning"});
+});
+
+// app.listen(PORT, function (err) {
+//     if (err) console.log(err);
+//     console.log("Server listening on PORT", PORT);
+// });
+
+// const pool = mysql.createPool({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.INSTANCE_CONNECTION_NAME,
+//     port: process.env.DB_PORT
+// });
